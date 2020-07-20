46e208f8a277ea84b1db134b6523d27300c6b613
tran.thu.trangb <tran.thu.trangb@sun-asterisk.com>
2020-07-20 10:10:15 +0700

    grid template

---
+++ src/components/Container.js
@@ -0,0 +1,7 @@
+import React from "react"
+import { ContainerWrapper } from "../elements"
+
+//grio template for the whole app
+export const Container = ({ children }) => {
+  return <ContainerWrapper>{children}</ContainerWrapper>
+}
